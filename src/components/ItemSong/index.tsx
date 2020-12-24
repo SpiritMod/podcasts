574282ea8384e1fc4e0c33cccfabbc8f08f3dbac
SpiritMod <sashko.harchuk@gmail.com>
2020-12-24 19:02:29 +0200

    added change player theme

--- src/components/ItemSong/index.tsx
+++ src/components/ItemSong/index.tsx
@@ -30,12 +30,17 @@ const ItemSong: React.FC<ItemSong> = (props) => {
     const className = props.className;
     const dataKey = props.dataKey;
 
+    const handlerClick = () => {
+        document.documentElement.style.setProperty('--color-player-a', colorFirst);
+        document.documentElement.style.setProperty('--color-player-b', colorSecond);
+    }
+
     return (
         <div className={`${styles.song} ${className}`} data-key={dataKey}>
-            <div className={styles.border} style={{background: `linear-gradient(to bottom, ${colorFirst}, ${colorSecond})`}}></div>
+            <div className={styles.border} style={{background: `linear-gradient(to bottom, ${colorFirst}, ${colorSecond})`}} />
             <div className={styles.content}>
                 <a href={podcastUrl} target="_blank" className={styles.img}>
-                    <img src={imgUrl}/>
+                    <img src={imgUrl} />
                 </a>
                 <div className={styles.info}>
                     <div className={styles.header}>
@@ -43,11 +48,10 @@ const ItemSong: React.FC<ItemSong> = (props) => {
                         <a href={epizodeUrl} className={styles.name}>{epizode}</a>
                         <a href={epizodeUrl} className={styles.description}>{description}</a>
                     </div>
-                    <div className={styles.controls}>
+                    <div className={styles.controls} onClick={handlerClick}>
                         <span>Слушать</span>
-                        <div className={styles.play}
-                             style={{background: `linear-gradient(140deg, ${colorFirst}, ${colorSecond})`}}>
-                            <span className="icon-play"></span>
+                        <div className={styles.play} style={{background: `linear-gradient(140deg, ${colorFirst}, ${colorSecond})`}}>
+                            <span className="icon-play" />
                         </div>
                     </div>
                 </div>

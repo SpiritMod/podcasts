0c645ded942a0f7e85c7e5ee7cac0d357a1c92b8
Ivanna Humeniuk <ivanna.humeniuk@pm.bet>
2020-12-28 11:54:16 +0200

    Added sort active

--- src/components/Sort/index.tsx
+++ src/components/Sort/index.tsx
@@ -4,23 +4,21 @@ import React from "react";
 //styles
 import styles from './styles/styles.module.scss';
 
-
 const Sort: React.FC = () => {
+
   return (
       <div className={styles.sort}>
           <div className={styles.text} >Сначала новые</div>
           <div className={styles.nav}>
-              <div className={styles.icon}>
-                  <span className="icon-arrow-short-right"></span>
+              <div className={`${styles.icon} ${styles.active}`}>
+                  <span className="icon-arrow-short-right"/>
               </div>
               <div className={styles.icon}>
-                  <span className="icon-arrow-short-left"></span>
+                  <span className="icon-arrow-short-left"/>
               </div>
           </div>
       </div>
   )
 };
-
-
 
 export default Sort;
